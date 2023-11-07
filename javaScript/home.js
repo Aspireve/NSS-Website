7ee67591cbaa286ed61544c2b36844e4c21e4642
Jitesh dubey <jiteshdubey001@gmail.com>
2023-11-07 18:56:25 +0530

    New change

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,11 +1,28 @@
+let dispvalue = document.querySelectorAll(".num");
+let Interval = 5000;
+
+dispvalue.forEach((dispvalue) => {
+  let startValue = 0;
+  let endValue = parseInt(dispvalue.getAttribute("data-val"));
+  let duration = Math.floor(Interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    dispvalue.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
 var a = 0;
 var tl = gsap.timeline();
-time = () => {
-  setInterval(function () {
-    a = a + Math.floor(Math.random() * 15);
-  }, 50);
-};
-time();
+// time = () => {
+//   setInterval(function () {
+//     a = a + Math.floor(Math.random() * 15);
+//   }, 100);
+// };
+// time();
+
 tl.from("#loader img", {
   scale: 0,
   opacity: 0,
@@ -19,7 +36,7 @@ tl.from("#loader h1", {
   stagger: 0.3,
 });
 tl.to("#loader", {
-  //   opacity: 0,
+  opacity: 0,
   duration: 1,
   display: "none",
 });
