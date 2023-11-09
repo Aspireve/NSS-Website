f5c6ca12bf60cdf2f2e0a1775aca22aa9d02b350
Jitesh dubey <jiteshdubey001@gmail.com>
2023-11-10 02:25:54 +0530

    xhanged

--- javaScript/home.js
+++ javaScript/home.js
@@ -1,27 +1,26 @@
 let dispvalue = document.querySelectorAll(".num");
 let Interval = 5000;
 
-dispvalue.forEach((dispvalue) => {
-  let startValue = 0;
-  let endValue = parseInt(dispvalue.getAttribute("data-val"));
-  let duration = Math.floor(Interval / endValue);
-  let counter = setInterval(function () {
-    startValue += 1;
-    dispvalue.textContent = startValue;
-    if (startValue == endValue) {
-      clearInterval(counter);
-    }
-  }, duration);
-});
+counter = () => {
+  dispvalue.forEach((dispvalue) => {
+    let startValue = 0;
+    let endValue = parseInt(dispvalue.getAttribute("data-val"));
+    let duration = Math.floor(Interval / endValue);
+    let counter = setInterval(
+      (increment = () => {
+        startValue += 1;
+        dispvalue.textContent = startValue;
+        if (startValue == endValue) {
+          clearInterval(counter);
+        }
+      }),
+      duration
+    );
+  });
+};
+// counter();
 
-var a = 0;
 var tl = gsap.timeline();
-// time = () => {
-//   setInterval(function () {
-//     a = a + Math.floor(Math.random() * 15);
-//   }, 100);
-// };
-// time();
 
 tl.from("#loader img", {
   scale: 0,
@@ -39,4 +38,8 @@ tl.to("#loader", {
   opacity: 0,
   duration: 1,
   display: "none",
+});
+ScrollTrigger.create({
+  trigger: ".volunteers-count",
+  onMouseEnter: counter(),
 });
