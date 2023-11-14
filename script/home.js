36e44e58c9c862c7eff62e4af2b3c5cf3433727d
Aspireve <steve1818fernandes@gmail.com>
2023-11-14 09:45:37 +0530

    Added Testimonials

--- script/home.js
+++ script/home.js
@@ -28,10 +28,6 @@ var r = document.querySelector(':root');
 
 body.addEventListener("scroll", () => {
     const currScroll = document.body.scrollTop
-    console.log(currScroll)
-    console.log("why not running")
-
-    console.log(currScroll)
 
    
     if(currScroll <= 10) {
@@ -110,3 +106,109 @@ hiddenElements.forEach((e) => observer.observe(e))
 //     }
 //   }, duration);
 // });
+
+// Testimonials
+
+/* Create an array to hold the different image positions */
+var itemPositions = [];
+
+var items = document.querySelectorAll("#scroller .item")
+
+var numberOfItems = items.length;
+console.log(numberOfItems)
+function assignPositions() {
+  for (var i = 0; i < numberOfItems; i++) {
+    if (i === 0) {
+      itemPositions[i] = "left-hidden";
+    } else if (i === 1) {
+      itemPositions[i] = "left";
+    } else if (i === 2) {
+      itemPositions[i] = "middle";
+    } else if (i === 3) {
+      itemPositions[i] = "right";
+    } else {
+      itemPositions[i] = "right-hidden";
+    }
+  }
+
+  items.forEach((e, idx) => {
+    console.log(e)
+    console.log(idx)
+    e.classList.add(itemPositions[idx])
+  })
+}
+
+
+
+
+function scroll(direction) {
+  if (direction === "prev") {
+    itemPositions.push(itemPositions.shift());
+  } else if (direction === "next") {
+    itemPositions.unshift(itemPositions.pop());
+  }
+  items.forEach((e, idx) => {
+    e.classList.remove("left-hidden")
+    e.classList.remove("left")
+    e.classList.remove("middle")
+    e.classList.remove("right")
+    e.classList.remove("right-hidden")
+    e.classList.add(itemPositions[idx])
+  })
+}
+
+// /* Do all this when the DOMs ready */
+
+assignPositions()
+var autoScroll = setInterval(() => {scroll('next')}, 4000);
+
+var scroller = document.getElementById("scroller");
+var nav = document.querySelector(".nav");
+
+// scroller.addEventListener("mouseout")
+
+scroller.addEventListener("mouseenter", () => {
+    console.log("entered")
+    clearInterval(autoScroll)
+    nav.style.display = "block"
+})
+scroller.addEventListener("mouseleave", () => {
+    console.log("left")
+    autoScroll = setInterval(() => {scroll('next')}, 4000);
+    nav.style.display = "none"
+})
+
+// document.querySelectorAll("#scroller")[0].style.hover
+//   $("#scroller").hover(
+//     function () {
+//       window.clearInterval(autoScroll);
+//       $(".nav").stop(true, true).fadeIn(200);
+//     },
+//     function () {
+//       autoScroll = window.setInterval("scroll('next')", 4000);
+//       $(".nav").stop(true, true).fadeOut(200);
+//     }
+//   );
+// document.readyState(function () {
+//   assignPositions();
+//   var autoScroll = window.setInterval("scroll('next')", 4000);
+
+//   /* Hover behaviours */
+//   $("#scroller").hover(
+//     function () {
+//       window.clearInterval(autoScroll);
+//       $(".nav").stop(true, true).fadeIn(200);
+//     },
+//     function () {
+//       autoScroll = window.setInterval("scroll('next')", 4000);
+//       $(".nav").stop(true, true).fadeOut(200);
+//     }
+//   );
+
+document.querySelector(".prev").addEventListener("click", () => {
+    scroll("prev")
+})
+
+document.querySelector(".next").addEventListener("click", () => {
+    scroll("next")
+})
